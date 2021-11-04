e4e9d5f7e9f5bac3e0a36141b1087f852dff8e68
wuzhoujohn <wuzhoujohn@gmail.com>
2021-11-04 14:24:58 -0400

    update

--- src/main.cpp
+++ src/main.cpp
@@ -176,7 +176,7 @@ vector<double> getXY(double s, double d, vector<double> maps_s, vector<double> m
 
 	while(s > maps_s[prev_wp+1] && (prev_wp < (int)(maps_s.size()-1) ))
 	{
-		prev_wp++;
+		++prev_wp;
 	}
 
 	int wp2 = (prev_wp+1)%maps_x.size();
